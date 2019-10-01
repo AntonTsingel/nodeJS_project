a6387d8cc839f3704d8869e66e86e50a149cd23a
Anton Tsingel <atsingel@VRP.local>
2019-10-01 18:35:26 +0300

    initial connection to database

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Pool, Client } = require('pg')
+/*const { Pool, Client } = require('pg')
 
 const connectionString = 'postgres://osdtvtdq:CVBPgsz-w0p9wGShZ96JAl-ZBrirwZYv@salt.db.elephantsql.com:5432/osdtvtdq'
 const pool = new Pool({
@@ -16,6 +16,29 @@ client.query('SELECT NOW()', (err, res) => {
   console.log(err, res)
   client.end()
 })
+
+*/
+require('dotenv').config();
+var pg = require('pg');
+//or native libpq bindings
+//var pg = require('pg').native
+
+var conString = process.env.ELEPHANTSQL_URL || "postgres://osdtvtdq:CVBPgsz-w0p9wGShZ96JAl-ZBrirwZYv@salt.db.elephantsql.com:5432/osdtvtdq";
+
+var client = new pg.Client(conString);
+client.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query('SELECT NOW() AS "theTime"', function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].theTime);
+    client.end();
+  });
+});
+
 
 const express = require('express')
 const bodyParser = require('body-parser')
