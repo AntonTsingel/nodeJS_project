4e92038bb24a7a32274d05c36c0618e23d37e8ce
Anton Tsingel <atsingel@VRP.local>
2019-10-02 11:08:40 +0300

    removed express framework

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-/*const { Pool, Client } = require('pg')
-
-const connectionString = 'postgres://osdtvtdq:CVBPgsz-w0p9wGShZ96JAl-ZBrirwZYv@salt.db.elephantsql.com:5432/osdtvtdq'
-const pool = new Pool({
-  connectionString: connectionString,
-})
-pool.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  pool.end()
-})
-const client = new Client({
-  connectionString: connectionString,
-})
-client.connect()
-client.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  client.end()
-})
-
-*/
 require('dotenv').config();
 var pg = require('pg');
 //or native libpq bindings
@@ -39,7 +19,52 @@ client.connect(function(err) {
   });
 });
 
+const http = require('http');
 
+/** handle GET request */
+function getHandler(req, res, reqUrl) {
+    res.writeHead(200);
+    res.write('GET parameters: ' + reqUrl.searchParams);
+    res.end();
+}
+
+/** handle POST request */
+function postHandler(req, res, reqUrl) {
+    req.setEncoding('utf8');
+    req.on('data', (chunk) => {
+        res.writeHead(200);
+        res.write('POST parameters: ' + chunk);
+        res.end();
+    });
+}
+
+/** if there is no related function which handles the request, then show error message */
+function noResponse(req, res) {
+    res.writeHead(404);
+    res.write('Sorry, but we have no response..\n');
+    res.end();
+}
+
+http.createServer((req, res) => {
+    // create an object for all redirection options
+    const router = {
+        'GET/retrieve-data': getHandler,
+        'POST/send-data': postHandler,
+        'default': noResponse
+    };
+    // parse the url by using WHATWG URL API
+    let reqUrl = new URL(req.url, 'http://127.0.0.1/');
+    // find the related function by searching "method + pathname" and run it
+    let redirectedFunc = router[req.method + reqUrl.pathname] || router['default'];
+    redirectedFunc(req, res, reqUrl);
+}).listen(8080, () => {
+    console.log('Server is running at http://127.0.0.1:8080/');
+});
+
+
+
+
+/*
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
@@ -59,3 +84,26 @@ app.get('/', (request, response) => {
   app.listen(port, () => {
     console.log(`App running on port ${port}.`)
   })
+*/
+
+/*
+  const { Pool, Client } = require('pg')
+
+const connectionString = 'postgres://osdtvtdq:CVBPgsz-w0p9wGShZ96JAl-ZBrirwZYv@salt.db.elephantsql.com:5432/osdtvtdq'
+const pool = new Pool({
+  connectionString: connectionString,
+})
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  pool.end()
+})
+const client = new Client({
+  connectionString: connectionString,
+})
+client.connect()
+client.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  client.end()
+})
+
+*/
