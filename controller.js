46e3c25d47c4090698aafe7fb6f712aa85cb8c51
Anton Tsingel <atsingel@VRP.local>
2019-10-02 15:37:33 +0300

    minor syntax changes

--- controller.js
+++ controller.js
@@ -26,23 +26,16 @@ module.exports = http.createServer((req, res) => {
     const reqUrl = url.parse(req.url, true);
     // GET Endpoint
     if (reqUrl.pathname == '/sample' && req.method === 'GET') {
-        console.log('Request Type:' +
-            req.method + ' Endpoint: ' +
-            reqUrl.pathname);
+        console.log('Request Type:' + req.method + ' Endpoint: ' + reqUrl.pathname);
         service.sampleRequest(req, res);
     // POST Endpoint
     } else if (reqUrl.pathname == '/test' && req.method === 'POST') {
-        console.log('Request Type:' +
-            req.method + ' Endpoint: ' +
-            reqUrl.pathname);
-
+        console.log('Request Type:' + req.method + ' Endpoint: ' + reqUrl.pathname);
         service.testRequest(req, res);
 
     } else {
         console.log('Request Type:' +
-            req.method + ' Invalid Endpoint: ' +
-            reqUrl.pathname);
-
+            req.method + ' Invalid Endpoint: ' + reqUrl.pathname);
         service.invalidRequest(req, res);
 
     }
